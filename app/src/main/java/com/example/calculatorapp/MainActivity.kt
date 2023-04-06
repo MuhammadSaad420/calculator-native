2129853aecec518972e6a9a0d8f63ad7ea0a99a3
MuhammadSaad420 <saad.oraxtech@gmail.com>
2023-04-06 17:32:39 +0500

    Using OnClick and Implementing the CLR Functionality

--- app/src/main/java/com/example/calculatorapp/MainActivity.kt
+++ app/src/main/java/com/example/calculatorapp/MainActivity.kt
@@ -2,10 +2,22 @@ package com.example.calculatorapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
+    private var outputText: TextView? = null
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        outputText = findViewById(R.id.outputText);
+    }
+    fun digit(view: View) {
+      outputText?.append((view as Button).text);
+    }
+    fun onClear(view: View) {
+        outputText?.text ="";
     }
 }
