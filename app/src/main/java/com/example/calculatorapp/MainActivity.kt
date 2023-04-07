58031407f844248c75d25dd18678dbea92f2b1fd
MuhammadSaad420 <saad.oraxtech@gmail.com>
2023-04-07 11:09:53 +0500

    Adding the onDeciamalPoint Function & Adding the onOoperator Functionality using Contains and StartsWith on Strings

--- app/src/main/java/com/example/calculatorapp/MainActivity.kt
+++ app/src/main/java/com/example/calculatorapp/MainActivity.kt
@@ -8,6 +8,8 @@ import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
     private var outputText: TextView? = null
+    private var hasDot = false;
+    private var lastNumeric = false
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
@@ -16,8 +18,39 @@ class MainActivity : AppCompatActivity() {
     }
     fun digit(view: View) {
       outputText?.append((view as Button).text);
+        lastNumeric = true;
     }
     fun onClear(view: View) {
         outputText?.text ="";
+         hasDot = false;
+         lastNumeric = false
     }
+
+    fun onDot(view: View) {
+        if(lastNumeric && !hasDot) {
+            outputText?.append(".");
+            hasDot = true;
+        }}
+        fun onOperator(view: View) {
+            if(lastNumeric && !isOperatorAdded(outputText?.text.toString())) {
+                outputText?.append((view as Button).text)
+                lastNumeric = false;
+            }
+        }
+
+
+        fun isOperatorAdded(value: String) : Boolean {
+            if(value.startsWith("-")) {
+                false;
+            }
+            else if(value.contains("+")
+                || value.contains("-")
+                || value.contains("*")
+                || value.contains("/")) {
+                return true;
+                }
+            return  false;
+        }
+
+
 }
